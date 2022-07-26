11ee0047709a4139229f440cca480ca99979ecc2
silverzx <silverzx713666@gmail.com>
2022-07-26 09:58:41 -0400

    prototype prototyped (day 1)

--- AccompanyingJS.js
+++ AccompanyingJS.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { TetrahedronGeometry } from 'three';
 
 // init
 
@@ -7,11 +8,15 @@ camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const geometry = new THREE.BoxGeometry(0.45, 0.2, 0.2);
+const tetraGeometry = new THREE.TetrahedronGeometry(.3, 1);
 const material = new THREE.MeshNormalMaterial();
+const tetraMaterial = new THREE.MeshNormalMaterial();
 
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const tetraMesh = new THREE.Mesh(tetraGeometry, tetraMaterial);
+scene.add(tetraMesh);
+// scene.add(mesh);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -19,12 +24,23 @@ renderer.setAnimationLoop(animation);
 document.body.appendChild(renderer.domElement);
 
 // animation
+let tweakSpeed = 5000;
 
 function animation(time) {
 
     mesh.rotation.x = time / 2000;
     mesh.rotation.y = time / 1000;
 
+    tetraMesh.rotation.x = time / tweakSpeed;
+    tetraMesh.rotation.y = time / tweakSpeed * 2;;
+
     renderer.render(scene, camera);
 
 }
+
+document.addEventListener('keydown', function(event) { //*to detect singular input
+    if (event.key == "Space") {
+        tweakSpeed /= 4;
+        console.log("tweaking");
+    }
+});
